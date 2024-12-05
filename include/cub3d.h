4dad34ca4bee18cce94230464ee7645f7eb1a605
Jeanne Wu <jewu@paul-f5Br6s5.clusters.42paris.fr>
2024-12-05 17:52:38 +0100

    added wall textures

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,23 +6,37 @@
 /*   By: jewu <jewu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 13:25:04 by jewu              #+#    #+#             */
-/*   Updated: 2024/12/05 13:30:58 by jewu             ###   ########.fr       */
+/*   Updated: 2024/12/05 17:51:17 by jewu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+// **********************************
+// *      C language Libraries      *
+// **********************************
+
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <errno.h>
 # include <sys/time.h>
 # include <math.h>
 # include "libft.h"
 # include "mlx.h"
 # include <X11/Xlib.h>
 # include <X11/keysym.h>
+
+// **********************************
+// *          Game assets           *
+// **********************************
+
+# define OAK_TREE		"assets/oak_tree.xpm"
+# define OLD_WOOD		"assets/old_wood.xpm"
+# define PLYWOOD		"assets/plywood.xpm"
+# define PLANT			"assets/twisted_plant.xpm"
 
 #endif
