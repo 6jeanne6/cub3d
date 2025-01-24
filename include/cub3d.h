a3dd6a5563646c4bf20eb8795ffcbbc19073da01
Jeanne Wu <jewu@paul-f5Br1s3.clusters.42paris.fr>
2025-01-24 14:43:35 +0100

    feature: execution

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: jewu <jewu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 13:25:04 by jewu              #+#    #+#             */
-/*   Updated: 2025/01/24 13:00:12 by jewu             ###   ########.fr       */
+/*   Updated: 2025/01/24 14:37:17 by jewu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,15 +31,6 @@
 # include <X11/keysym.h>
 # include <stdbool.h>
 
-# define NO 2
-# define SO 3
-# define WE 4
-# define EA 5
-# define F 6
-# define C 7
-# define FAILURE 1
-# define SUCCESS 0
-# define ERROR_MAP 2
 # define VALID_MAP_CHARS "01NSEW"
 
 # define WIDTH 900
@@ -47,17 +38,39 @@
 # define TILE_SIZE 64
 # define FOV 60
 # define ROT_SPEED 0.01
+# define PLAYER_SPEED 5
 # define PI 3.14159265359
-# define GO_LEFT 42
-# define GO_RIGHT 43
-# define GO_UP 44
-# define GO_DOWN 45
-# define ROTATE_LEFT 46
-# define ROTATE_RIGHT 47
 
 /* ****************************** */
 /*          Structures            */
 /* ****************************** */
+
+enum	e_error
+{
+	SUCCESS,
+	FAILURE,
+	ERROR_MAP
+};
+
+enum	e_texture
+{
+	NO,
+	SO,
+	WE,
+	EA,
+	F,
+	C
+};
+
+enum	e_movement
+{
+	GO_LEFT = 42,
+	GO_RIGHT,
+	GO_UP,
+	GO_DOWN,
+	ROTATE_LEFT,
+	ROTATE_RIGHT
+};
 
 typedef struct s_ray
 {
@@ -91,11 +104,11 @@ typedef struct s_image
 
 	int					width;
 	int					height;
-	int					id;
 	int					bits_per_pixel;
 	int					endian;
 	int					tile_size;
 	int					size_line;
+	int					id;
 }						t_image;
 
 typedef struct s_info
@@ -148,11 +161,6 @@ bool	line_with_only_whitespace(char *line);
 
 void	get_player_position(t_info *info);
 
-/*Initialization*/
-int		init_everything(t_info *info);
-int		init_player(t_info *info, t_player *player);
-int		handle_keypress(int key, t_info *info);
-
 /*Raycasting*/
 void	raycasting(t_info *info, t_ray *ray);
 
@@ -172,18 +180,21 @@ void	super_mlx_pixel_put(t_image *texture, int x, int y, int color);
 float	get_x_gap(t_info *info, t_image *texture);
 
 t_image	*get_wall_texture_rendering(t_info *info, int hori_flag);
-t_image	*which_cardinal_direction(t_info *info, char *direction);
+t_image	*which_cardinal_direction(t_info *info, int direction);
 
-int		the_texture_color(t_image *texture, float x_gap, float y_gap);
+int		put_texture_color(t_image *texture, float x_gap, float y_gap);
+int		the_texture_rgb(int red, int blue, int green);
 
 /*Execution*/
-void    lets_execute_the_game(t_info *info);
+void	lets_execute_the_game(t_info *info);
 void	keyboard_hook(t_info *info);
 void	the_player_moves(t_info *info, double x_move, double y_move);
-void	the_player_rotates(t_info *info, double x_move, double y_move, int right);
-void    i_can_move(t_info *info, double x_move, double y_move);
+void	the_player_rotates(t_info *info, int right);
+void	i_can_move(t_info *info, double x_move, double y_move);
+void	free_textures(t_info *info);
 
 int		handle_keypress(int key, t_info *info);
+int		init_window(t_info *info);
 
 /* Error */
 void	error(char *msg);
